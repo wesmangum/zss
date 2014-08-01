aa3c51ebba2d3210da6ecc8ed0c38814ac83d283
Eliza Brock <eliza@elizabrocksoftware.com>
2014-08-01 13:27:03 -0500

    Add skills to a training path.

--- spec/integration/adding_skills_to_training_paths_spec.rb
+++ spec/integration/adding_skills_to_training_paths_spec.rb
@@ -1,14 +1,11 @@
 RSpec.describe "Adding skills to a training path" do
-  before do
-    skip
-  end
   context "valid input" do
     let!(:training_path1){ TrainingPath.create(name: "Weapon Skills") }
     let!(:training_path2){ TrainingPath.create(name: "Running") }
     let!(:output){ run_zss_with_input("2", "add", "Jogging") }
 
     it "prints a success message" do
-      expect(output).to include("Jogging has been added to the Running training path")
+     expect(output).to include("Jogging has been added to the Running training path")
     end
 
     it "saves the correct training path to the record" do
@@ -22,10 +19,10 @@ RSpec.describe "Adding skills to a training path" do
   end
   context "invalid input" do
     let!(:training_path){ TrainingPath.create(name: "Running") }
-    let!(:output){ run_zss_with_input("1", "add", "44") }
+    let!(:output){ run_zss_with_input("1", "add", "") }
 
     it "prints an error message" do
-      expect(output).to include("Skill name can't be a number")
+      expect(output).to include("name cannot be blank")
     end
 
     it "doesn't create a skill" do
