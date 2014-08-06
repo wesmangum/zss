53b9cc8e3bcc63c66c0e82b432fc5213bc51a96e
Eliza Brock <eliza@elizabrocksoftware.com>
2014-08-06 15:20:33 -0500

    Add description to skills.
    Note: This fixes the intentional `rake` failure from the previous
    commit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806150800) do
+ActiveRecord::Schema.define(version: 20140806151600) do
 
   create_table "skills", force: true do |t|
     t.string  "name"
     t.integer "training_path_id"
+    t.text    "description"
   end
 
   create_table "training_paths", force: true do |t|
